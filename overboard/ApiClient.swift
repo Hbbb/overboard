5ea70e93114cf803096eb0b9f20a04224e24ffec
Harrison <hborges005@gmail.com>
2018-06-06 19:50:30 -0400

    add api client and config

---
+++ overboard/ApiClient.swift
@@ -0,0 +1,36 @@
+//
+//  ApiClient.swift
+//  overboard
+//
+//  Created by Harrison Borges on 6/6/18.
+//  Copyright © 2018 ping. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct ApiClient {
+  func getBoard() {
+    let boardUrl = Config.Urls.local + "/boards/1.json"
+    request(url: boardUrl, method: .get)
+  }
+  
+  private func request(url: String, method: HTTPMethod) {
+    var headers = [String: String]()
+    headers["Content-Type"] = "application/json"
+    headers["Accept"] = "application/json"
+    
+    Alamofire.request(url, method: method, headers: headers).responseJSON { response in
+      print("Response: \(String(describing: response.response))") // http url response
+      print("Result: \(response.result)")                         // response serialization result
+      
+      if let json = response.result.value {
+        print("JSON: \(json)") // serialized json response
+      }
+      
+      if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+        print("Data: \(utf8Text)") // original server data as UTF8 string
+      }
+    }
+  }
+}
---
+++ overboard/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  overboard
+//
+//  Created by Harrison Borges on 6/6/18.
+//  Copyright © 2018 ping. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+  struct Urls {
+    static let local = "http://localhost:3000"
+  }
+}
